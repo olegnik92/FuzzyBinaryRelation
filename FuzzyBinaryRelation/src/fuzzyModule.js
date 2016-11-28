2224e41688f280844125eaab15146ffac57cbaf7
olegnik92 <olegnik92@mail.ru>
2016-11-28 11:39:19 +0500

    optimizastion

--- FuzzyBinaryRelation/src/fuzzyModule.js
+++ FuzzyBinaryRelation/src/fuzzyModule.js
@@ -3,9 +3,9 @@ import ngMaterial from 'angular-material';
 
 const fuzzyModule = angular.module('fuzzy', [ngMaterial]);
 fuzzyModule.config(['$compileProvider', '$mdThemingProvider', function ($compileProvider, $mdThemingProvider) {
-   // $compileProvider.debugInfoEnabled(false);
-   // $compileProvider.commentDirectivesEnabled(false);
-   // $compileProvider.cssClassDirectivesEnabled(false);
+    $compileProvider.debugInfoEnabled(false);
+    $compileProvider.commentDirectivesEnabled(false);
+    $compileProvider.cssClassDirectivesEnabled(false);
     $mdThemingProvider.theme('default').accentPalette('red');
 }]);
 
